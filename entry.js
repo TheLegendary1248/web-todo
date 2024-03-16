f6b7deed6b04961271df2ede7043ad37546feb5a
TheLegendary1248 <1248alex@gmail.com>
2024-03-16 02:21:41 -0400

    this works??

--- entry.js
+++ entry.js
@@ -1,20 +1,60 @@
 import van from "vanjs-core"
+import { reactive } from "vanjs-ext"
 import "./style.css"
-const { div, p, span, button,textarea, br} = van.tags
-let outsideUpdate = false
-const task = () => {
-    const text = van.state("")
+const { div, p, input, span, button,textarea} = van.tags
+let dummy = 
+{
+    title: "Hello Title",
+    task: [
+        {
+            text:"Do dishes",
+            task: false,
+            weight: 2
+        },
+        {
+            text: "Cook",
+            task: [
+                {
+                    text: "Eggs",
+                    task: true,
+                },
+                {
+                    text: "Bacon",
+                    task: false,
+                    weight: 4
+                }
+            ],
+        },
+        {
+            text: "Die from doing this challenge",
+            task: true
+        }
+    ]
+}
+const title = van.state("TITLE")
+const task = (state) => {
+    const text = van.state(state.text)
+    console.log(text)
     return div({class: "task"},
         textarea({value: text, oninput: e => {let elem = e.target; text.val = elem.value; elem.style.height = ""; elem.style.height = elem.scrollHeight + 3 + "px"}}),
         div("NEST"),
-        progress)
+        progress())
 }
 const progress = () => {
     return div({class: "bar"}, span("VALUE"))
 }
-const toplayer = () => {
-
-    return task()
+/**
+ * 
+ * @param {Array} taskArr 
+ */
+const forEachTask = (taskArr) => {
+    taskArr.map(x => task(x))
+}
+const toplayer = (JSONstate) => {
+    const state = reactive(JSONstate)
+    console.log(state.task)
+    return div({id:"toplayers"},input({value: state.title, id:"title"}),
+    state.task.map(x => task(x)))
 }
 const set = () => {
 
@@ -23,4 +63,4 @@ const fontselect = () => {
     return div("FONT SELECT")
 }
 document.querySelectorAll(".f-sel").forEach(x => van.add(x, fontselect))
-van.add(document.body, toplayer, prefs)
+van.add(document.body, toplayer(dummy), prefs)
